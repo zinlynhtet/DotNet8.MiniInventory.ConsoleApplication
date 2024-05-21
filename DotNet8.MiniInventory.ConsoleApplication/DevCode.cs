6fff4d6720a327fdbf0ad37c763424750720004f
Zin Linn Htet <mackk5504@gmail.com>
2024-05-21 13:33:53 +0630

    Complete DevCode

---
+++ DotNet8.MiniInventory.ConsoleApplication/DevCode.cs
@@ -0,0 +1,14 @@
+﻿namespace DotNet8.MiniInventory.ConsoleApplication;
+
+public static class DevCode
+{
+    public static int ToInt(this object item)
+    {
+        return Convert.ToInt32(item);
+    }
+
+    public static decimal ToDecimal(this object item)
+    {
+        return Convert.ToDecimal(item);
+    }
+}
